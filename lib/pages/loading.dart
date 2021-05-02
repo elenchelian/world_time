5fcefa52421466cc046f76cd89d15122de1acb0d
elenchelian <elenchelianelen@gmail.com>
2021-05-03 00:24:02 +0800

    Lab 5

---
+++ lib/pages/loading.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:lab_5/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:http/http.dart';
+
+
+class Loading extends StatefulWidget {
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+
+
+
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(location: 'Berlin',flag:'germany.png',url:'Europe/Berlin');
+    await instance.getTime();
+    Navigator.pushReplacementNamed(context, '/home',arguments: {
+      'location':instance.location,
+      'flag':instance.flag,
+      'time':instance.time,
+      'isDayTime':instance.isDayTime,
+    });
+
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupWorldTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[900],
+      body:Center(
+        child: SpinKitFoldingCube(
+          color: Colors.white,
+          size: 50.0,
+        ),
+    ),
+    );
+  }
+}
